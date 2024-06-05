f9b21407a220e1f196ff39bd73eee1f234654576
echo <1225318858@qq.com>
2024-06-05 14:52:54 +0800

    删除拦截器

--- src/main/java/example/springmovie/security/interceptors/SaTokenConfigure.java
+++ src/main/java/example/springmovie/security/interceptors/SaTokenConfigure.java
@@ -9,17 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SaTokenConfigure implements WebMvcConfigurer {
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        // 注册 Sa-Token 拦截器，定义详细认证规则
-        registry.addInterceptor(new SaInterceptor(handler -> {
-            //只有登录才可以访问网站
-            SaRouter
-                    .match("/**")                               // 拦截的 path 列表，可以写多个 */
-                    .notMatch("/user/login","/user/register")   // 排除掉的 path 列表，可以写多个
-                .check(r -> StpUtil.checkLogin());                        // 要执行的校验动作，可以写完整的 lambda 表达式
-
-        })).addPathPatterns("/**");
-    }
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        // 注册 Sa-Token 拦截器，定义详细认证规则
+//        registry.addInterceptor(new SaInterceptor(handler -> {
+//            //只有登录才可以访问网站
+//            SaRouter
+////                    .match("/**")                               // 拦截的 path 列表，可以写多个 */
+////                    .notMatch("/user/login","/user/register")   // 排除掉的 path 列表，可以写多个
+//                .check(r -> StpUtil.checkLogin());                        // 要执行的校验动作，可以写完整的 lambda 表达式
+//
+//        })).addPathPatterns("/**");
+//    }
 }
 
