3fd43575fdfcc94c920cf81a57caa6cec7b5c7e2
Markbarkar <121151624+Markbarkar@users.noreply.github.com>
2024-06-13 10:54:35 +0800

    前端更改 06-13

--- src/main/java/example/springmovie/controller/MovieController.java
+++ src/main/java/example/springmovie/controller/MovieController.java
@@ -6,6 +6,7 @@ import example.springmovie.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 //@RestController
@@ -25,16 +26,20 @@ public class MovieController {
 //    在postman中输入下列网址进行访问，其中如果有10页，访问了第十一页，那么返回的依然是第十页的数据，在编代码时要注意这一点。
     @GetMapping("/popular")
 //    按照播放量分类展示，按照周播放量来排序决定，即example.springmovie.entity.Movie中的weekly_plays
-//    测试：http://localhost:8080/movies/popular?page=1&size=10
-    public PageInfo<Movie> getMoviesByPopularity(@RequestParam int pageNum, @RequestParam int pageSize) {
-        return movieService.getMoviesByPopularity(pageNum, pageSize);
+//    测试：http://localhost:8080/movies/popular?pageNum=1&pageSize=10
+    public String getMoviesByPopularity(@RequestParam int pageNum, @RequestParam int pageSize, Model model) {
+        System.out.println(movieService.getMoviesByPopularity(pageNum,pageSize).getList().get(0));
+        model.addAttribute("movies", movieService.getMoviesByPopularity(pageNum, pageSize).getList());
+        return "popular";
     }
 
     @GetMapping("/genre")
 //    按照类型分页展示，根据类型，即genre进行搜索，分页展示类型相同的
-//    测试：http://localhost:8080/movies/genre?genre="犯罪"&page=1&size=10
-    public PageInfo<Movie> getMoviesByGenre(@RequestParam String genre, @RequestParam int pageNum, @RequestParam int pageSize) {
-        return movieService.getMoviesByGenre(genre, pageNum, pageSize);
+//    测试：http://localhost:8080/movies/genre?genre=科幻&pageNum=1&pageSize=10
+    public String getMoviesByGenre(@RequestParam String genre, @RequestParam int pageNum, @RequestParam int pageSize, Model model) {
+        System.out.println(movieService.getMoviesByGenre(genre, pageNum, pageSize).getList());
+        model.addAttribute("reslist", movieService.getMoviesByGenre(genre, pageNum, pageSize).getList());
+        return "movies";
     }
 
 //    按照地区分类展示，根据类型搜索，即region进行搜索，分页展示地区相同的
